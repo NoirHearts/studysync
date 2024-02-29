742d9dd3a280390ec5682a34828cdf9869df2c97
efgmarquez <elmarquez@up.edu.ph>
2024-02-29 19:47:02 +0800

    test: made test descriptions more descriptive

--- tests/ui-tests.spec.ts
+++ tests/ui-tests.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from './fixtures';
 
-test.describe('sidepanel', () => {
+test.describe('testing initial UI', () => {
   test.beforeEach(async ({ page, extensionId }) => {
     await page.goto(`chrome-extension://${extensionId}/panel.html`);
   });
@@ -11,47 +11,46 @@ test.describe('sidepanel', () => {
     );
   });
 
-
-  test('dictionary content hidden', async ({ page }) => {
+  test('dictionary content hidden by default', async ({ page }) => {
     await expect(page.locator('.dictionary-content')).toBeHidden();
   });
 
-  test('dictionary becomes visible when clicked', async ({ page }) => {
+  test('dictionary content becomes visible when clicked', async ({ page }) => {
     await page.getByText('Dictionary', { exact: true }).click();
     await expect(page.locator('.dictionary-content')).toBeVisible();
   });
 
-  test('hide dictionary', async ({ page }) => {
+  test('hide dictionary when dictionary content is visible', async ({ page }) => {
     await page.getByText('Dictionary', { exact: true }).click();
     await page.getByText('Dictionary', { exact: true }).click();
     await expect(page.locator('.dictionary-content')).toBeHidden();
   });
 
-  test('notes content hidden', async ({ page }) => {
+  test('notes content hidden by default', async ({ page }) => {
     await expect(page.locator('.notes-content')).toBeHidden();
   });
 
-  test('notes becomes visible when clicked', async ({ page }) => {
+  test('notes content becomes visible when clicked', async ({ page }) => {
     await page.getByText('Notes', { exact: true }).click();
     await expect(page.locator('.notes-content')).toBeVisible();
   });
 
-  test('hide notes', async ({ page }) => {
+  test('hide notes when notes content is visible', async ({ page }) => {
     await page.getByText('Notes', { exact: true }).click();
     await page.getByText('Notes', { exact: true }).click();
     await expect(page.locator('.notes-content')).toBeHidden();
   });
 
-  test('to do list content hidden', async ({ page }) => {
+  test('to-do list content hidden by default', async ({ page }) => {
     await expect(page.locator('.tasks-content')).toBeHidden();
   });
 
-  test('to do list becomes visible when clicked', async ({ page }) => {
+  test('to-do list content becomes visible when clicked', async ({ page }) => {
     await page.getByText('To-Do List').click();
     await expect(page.locator('.tasks-content')).toBeVisible();
   });
 
-  test('hide to do list', async ({ page }) => {
+  test('hide to-do list when to-do list is visible', async ({ page }) => {
     await page.getByText('To-Do List').click();
     await page.getByText('To-Do List').click();
     await expect(page.locator('.tasks-content')).toBeHidden();
