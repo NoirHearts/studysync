ba27379d8b82763d48cbdc33615e53415b48b677
rudnam <francisalbarracin2@gmail.com>
2024-03-03 13:47:30 +0800

    fix: Fix bugs regarding file names and tests

--- src/pages/Panel/Panel.tsx
+++ src/pages/Panel/Panel.tsx
@@ -11,17 +11,21 @@ import Pomodoro from './subpanel/Pomodoro';
 
 const Panel: React.FC = () => {
   return (
-    <div className='outer-container'>
+    <div className="outer-container">
       <div className="container">
         <h3 className="name-header">StudySync</h3>
 
         <Pomodoro />
 
         <div className="other-features">
-
           <div id="tasks-container" className="feature-container">
             {/* <!-- <button id="tasks-btn" />Tasks</button> --> */}
-            <input id="tasks-btn" className="feature-button" type="checkbox" name="feat3" />
+            <input
+              id="tasks-btn"
+              className="feature-button"
+              type="checkbox"
+              name="feat3"
+            />
             <label htmlFor="tasks-btn">
               <img className="icons" src={tasksImg} /> To-Do List
             </label>
@@ -32,7 +36,12 @@ const Panel: React.FC = () => {
 
           <div id="notes-container" className="feature-container">
             {/* <!-- <button id="notes-btn">Notes</button> --> */}
-            <input id="notes-btn" className="feature-button" type="checkbox" name="feat2" />
+            <input
+              id="notes-btn"
+              className="feature-button"
+              type="checkbox"
+              name="feat2"
+            />
             <label htmlFor="notes-btn">
               <img className="icons" src={notesImg} /> Notes
             </label>
@@ -43,7 +52,12 @@ const Panel: React.FC = () => {
 
           <div id="dictionary-container" className="feature-container">
             {/* <!-- <button id="dict-btn">Dictionary</button> --> */}
-            <input id="dict-btn" className="feature-button" type="checkbox" name="feat1" />
+            <input
+              id="dict-btn"
+              className="feature-button"
+              type="checkbox"
+              name="feat1"
+            />
             <label htmlFor="dict-btn">
               <img className="icons" src={dictionaryImg} /> Dictionary
             </label>
@@ -51,7 +65,6 @@ const Panel: React.FC = () => {
               <Dictionary />
             </div>
           </div>
-
         </div>
       </div>
     </div>
