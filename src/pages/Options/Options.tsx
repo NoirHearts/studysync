7d9c1b8e30bb61e7434a46961cb4bb790ef4f600
rudnam <francisalbarracin2@gmail.com>
2024-03-10 16:42:30 +0800

    feat: Allow for importing/exporting of settings

--- src/pages/Options/Options.tsx
+++ src/pages/Options/Options.tsx
@@ -62,6 +62,58 @@ const Options: React.FC<Props> = ({ title }: Props) => {
     }
   };
 
+  const importSettings = () => {
+    const fileInput = document.createElement('input');
+    fileInput.type = 'file';
+    fileInput.accept = '.json';
+    fileInput.style.display = 'none';
+
+    fileInput.onchange = function (event) {
+      const target = event.target as HTMLInputElement;
+      const files = target.files;
+      if (!files || files.length === 0) return;
+
+      const file = files[0];
+      const reader = new FileReader();
+
+      reader.onload = function (e: ProgressEvent<FileReader>) {
+        const settingsJSON = e.target?.result as string;
+        const settings = JSON.parse(settingsJSON) as Settings;
+
+        try {
+          chrome.storage.sync.set(settings, () => {
+            displayStatus('Settings imported successfully.', 3000);
+            setSettings({ ...settings } as Settings);
+          });
+        } catch (error) {
+          console.error(error);
+          if (error instanceof Error) {
+            displayError(`Error: ${error.message}`, 5000);
+          } else {
+            displayError('Something went wrong.', 5000);
+          }
+        }
+      };
+
+      reader.readAsText(file);
+    };
+
+    document.body.appendChild(fileInput);
+    fileInput.click();
+  };
+
+  const exportSettings = () => {
+    chrome.storage.sync.get(null, function (items) {
+      const settingsJSON = JSON.stringify(items);
+      const blob = new Blob([settingsJSON], { type: 'application/json' });
+
+      const downloadLink = document.createElement('a');
+      downloadLink.download = 'studysync_settings.json';
+      downloadLink.href = URL.createObjectURL(blob);
+      downloadLink.click();
+    });
+  };
+
   return (
     <div className="options-container">
       <h1>{title} Page</h1>
@@ -92,6 +144,10 @@ const Options: React.FC<Props> = ({ title }: Props) => {
       <div className="error-message">{errorMessage}</div>
       <div className="status-message">{status}</div>
       <button onClick={saveSettings}>Save</button>
+      <div>
+        <button onClick={importSettings}>Import Settings</button>{' '}
+        <button onClick={exportSettings}>Export Settings</button>
+      </div>
     </div>
   );
 };
