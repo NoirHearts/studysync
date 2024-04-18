1502818afc2133b06342b63ddfcbec2357684097
Aikiro42 <hiroku042@gmail.com>
2024-04-18 20:59:50 +0800

    test: add test for saving note
    - added new test component NoteEditorTest
    - page.on('dialog', ...) confirmed to work

--- tests/Notes.error.spec.tsx
+++ tests/Notes.error.spec.tsx
@@ -3,9 +3,10 @@
 import { test, expect, MountResult } from '@playwright/experimental-ct-react';
 import React from 'react';
 import { NoteItemTest } from '../src/components/NoteItemTest'
-// import NoteEditor from '../src/components/NoteEditor'
+import { NoteEditorTest } from '../src/components/NoteEditorTest'
 
 test.use({ viewport: { width: 400, height: 876 } });
+const wait = (ms: number) => new Promise((res) => setTimeout(res, ms));
 
 test.describe('Notes', () => {
   
@@ -13,14 +14,40 @@ test.describe('Notes', () => {
 
     let component: MountResult;
   
-    test('feature handles errors when opening notes', async ({ mount }) => {
-      component = await mount(<NoteItemTest />)
+    test('feature handles errors when opening notes', async ({ page, mount }) => {
+      component = await mount(<NoteItemTest />);
+      page.on('dialog', async dialog => {
+        await expect(dialog.message() == 'Error Opening Note').toBeTruthy()
+        await dialog.dismiss()
+      });
       await component.click();
+    });
+
+    test('feature handles errors when saving notes', async ({ page, mount }) => {
+      component = await mount(<NoteEditorTest />);
+      page.on('dialog', async dialog => {
+        await expect(dialog.message() == 'Error Saving Note').toBeTruthy()
+        await dialog.dismiss()
+      });
+
+      // write title
+      await component.locator('#note-editor-title-input').fill('Lorem Ipsum');
+      // write content
+      await component
+        .locator('#note-editor-content-input')
+        .fill('Lorem ipsum dolor sit amet');
+
+      // wait for autosave
+      await wait(1000);
+      
+      // close note (must induce error)
+      await page.locator('#note-editor-back').click();
+
     });
   
     test('feature handles errors when deleting notes', async ({ page, mount }) => {
       component = await mount(<NoteItemTest />)
-      page.on('dialog', async dialog => { // not working?
+      page.on('dialog', async dialog => {
         await expect(dialog.message() == 'Error Deleting Note').toBeTruthy()
         await dialog.dismiss()
       })
